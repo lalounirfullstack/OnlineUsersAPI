d65c1f816e240584bec98848ffb117731693c99e
Lalo Aguirre <lalo.ed.aguirre@gmail.com>
2023-08-02 04:06:42 -0700

    Added Routes

--- online-users-api/src/app/app-routing.module.ts
+++ online-users-api/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {UserFormComponent} from "./components/user-form/user-form.component";
+import {UserViewComponent} from "./pages/user-view/user-view.component";
+import {C404Component} from "./pages/c404/c404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+   {path: "", pathMatch: 'full', redirectTo: 'home'},
+   {path: "home", component:HomeComponent},
+   {path: "newuser", component:UserFormComponent},
+   {path: "updateuser/:iduser", component: UserFormComponent},
+   {path: "viewuser/:iduser", component: UserViewComponent},
+   {path: "**", component: C404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
