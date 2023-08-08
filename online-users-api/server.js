e35068b01d46a6dfd66933f1073909004c407a4b
Lalo Aguirre <lalo.ed.aguirre@gmail.com>
2023-08-07 17:30:43 -0700

    Adjusted server.js

--- online-users-api/server.js
+++ online-users-api/server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/<app-online-users-api>'));
+app.use(express.static(__dirname + '/dist/online-users-api'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/<app-online-users-api>/index.html'));});
+'dist/online-users-api/index.html'));});
 app.listen(process.env.PORT || 8080);
