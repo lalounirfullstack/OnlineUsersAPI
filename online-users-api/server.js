3b4837eab8e91ba8291f25580176316c27178b0d
Lalo Aguirre <lalo.ed.aguirre@gmail.com>
2023-08-07 18:00:04 -0700

    Corrected server.js  error

--- online-users-api/server.js
+++ online-users-api/server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static(__dirname + '/dist/online-users-api'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'dist/online-users-api/index.html'));});
+'/dist/online-users-api/index.html'));});
 app.listen(process.env.PORT || 8080);
